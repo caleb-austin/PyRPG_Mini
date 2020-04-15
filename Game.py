54c6a7f6d9a039afe2df25fa83bc70eeba2aea10
caleb <caleb@macbook-pro.attlocal.net>
2020-04-15 18:48:21 -0400

    Rearranged some files and added a navbar for style

--- Game.py
+++ Game.py
@@ -4,14 +4,15 @@ import pickle
 import random
 import time
 from sqlite3 import connect
+
 import Enemy
 import Hero
 import dbsetup
 from texttools import *
 
-
 # game class makes the game work instantiates all other classes at some point.
-class Game:
+
+class Game():
     def __init__(self):
         # adds a little suspense
         # TODO: add suspense option to some printing methods?
@@ -22,7 +23,6 @@ class Game:
         self.debugging = 0
         centerprint('Debugging Mode? [1] for yes, [ENTER] for no')
         self.debugging = input()
-
         # riddle mode 0 - optional, 1 - mandatory
         centerprint('Riddles Mandatory? [1] for yes, [ENTER] for no')
         self.riddlemode = input()
