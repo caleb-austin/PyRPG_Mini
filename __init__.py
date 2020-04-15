bc29c4e35a2adcf1da563727695b876588573296
caleb <caleb@macbook-pro.attlocal.net>
2020-04-14 20:27:57 -0400

    Created the shell home page layout for the web application by integrating features of flask

--- __init__.py
+++ __init__.py
@@ -1,5 +1,13 @@
 import Game
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
 
 if __name__ == '__main__':
+    app.run(debug=True)
     ourgame = Game.Game()
     ourgame.gameloop()
